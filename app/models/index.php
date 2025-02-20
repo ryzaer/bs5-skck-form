e2324bc0243061e29ed0aef211e271c2f6c140ac
ryzaer <ryzaer@live.com>
2025-02-20 11:45:59 +0700

    view change

--- app/models/index.php
+++ app/models/index.php
@@ -1,5 +1,5 @@
 <?php $this->index = function($f3,$resp,$hndl){//=====  INDEX FUNCTION START HERE ==========>
 	
-	print "<b><i style=\"color:orange\">Route to $f3->PATH path is open!</i></b><br>";
+	$f3->view("index.html");
 	
 }?>
