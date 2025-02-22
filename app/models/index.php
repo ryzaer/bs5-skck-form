4264ed529a4fa7b00e166016bb15b27b67fbb68e
ryzaer <ryzaer@live.com>
2025-02-22 11:03:34 +0700

    ok

--- app/models/index.php
+++ app/models/index.php
@@ -1,5 +1,6 @@
 <?php $this->index = function($f3,$resp,$hndl){//=====  INDEX FUNCTION START HERE ==========>
 	
-	$f3->view("index.html");
+	$f3->view("index.htm");
+	$f3->scripts("test.js");
 	
 }?>
