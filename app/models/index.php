fd3a179095e0113407dfbff19bf0fd6c0d0f6616
ryzaer <ryzaer@live.com>
2025-02-24 15:18:46 +0700

    up bs5

--- app/models/index.php
+++ app/models/index.php
@@ -1,8 +1,5 @@
-<?php $this->index = function($f3,$resp,$hndl){//=====  INDEX FUNCTION START HERE ==========>
-	
+<?php $this->index = function($f3,$resp,$hndl){//=====  INDEX FUNCTION START HERE ==========>	
+		
 	$f3->view("spa_index.htm");
-	$f3->scripts("app.js");
-	$f3->styles("app.css");
-
 	
 }?>
